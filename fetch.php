4f0632ac8f2d1ba28686001100d07c0f53c9e9bf
Ken Irwin <ken@kenirwin.net>
2023-12-08 11:25:43 -0500

    added fetch.php to get google sheet data

---
+++ fetch.php
@@ -0,0 +1,20 @@
+<?php
+$gids = array(
+        "Art" => "296735077",
+        "Music" => "4719631",
+        "Math" => "791155157",
+        "Health" => "873386545",
+        "PhysEd" => "1107520403",
+        "Lang" => "972322472",
+        "Science" => "271009271",
+        "Social" => "1001060475",
+);
+$sheetId = '1Pt6VS4sVkgVdFl23WRNHtcjNDU6Gpuybme2fTQ38WdU';
+
+foreach ($gids as $filename => $gid) {
+    $sheetUrl = 'https://docs.google.com/spreadsheets/d/' . $sheetId . '/export?format=csv&id=' . $sheetId . '&gid=' . $gid;
+    $csv = file_get_contents($sheetUrl);
+    // print($csv);
+    $file = "./csv/" . $filename . ".csv";
+    file_put_contents($file, $csv);
+}
